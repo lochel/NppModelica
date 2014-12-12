881b0d6a7966fb1e4ec519a309993379fabb11cd
Lennart Ochel <lennart.ochel@fh-bielefeld.de>
2014-12-12 16:35:36 +0100

    Initial commit for NppModelica plugin
    Based on NppPlugin.NET package v0.6
    * http://notepad-plus-plus.org/contribute/plugin-howto.html
    * http://sourceforge.net/projects/sourcecookifier/files/other%20plugins

---
+++ Forms/frmMyDlg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NppModelica
+{
+    public partial class frmMyDlg : Form
+    {
+        public frmMyDlg()
+        {
+            InitializeComponent();
+        }
+    }
+}
